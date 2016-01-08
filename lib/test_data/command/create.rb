5954b25a6234e6c2149fc56c01bf707d78746ead
mizutaki <mizutaki4@gmail.com>
2016-01-09 00:14:31 +0900

    Modified to be able to file size specified

--- lib/test_data/command/create.rb
+++ lib/test_data/command/create.rb
@@ -2,11 +2,14 @@ require 'test_data/command'
 
 module TestData
   class Command < Thor
-  	desc "create [NUMBER]", "create test data of NUMBER"
-  	def create(number)
+  	desc "create [NUMBER] [SIZE]", "create test data of NUMBER SIZE"
+  	def create(number, size)
       number.to_i.times do |e|
         File.open(e.to_s + ".txt", "w") do |file|
-        	file.puts e
+          loop do
+            file.print 0
+            break if file.size == size.to_i
+          end
         end
       end
   	end
