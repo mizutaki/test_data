060d2b7f58ca34eca8f5dcc7d05f7406edda0fd7
mizutaki <mizutaki4@gmail.com>
2016-01-09 20:45:01 +0900

    refactoring

--- lib/test_data/command/create.rb
+++ lib/test_data/command/create.rb
@@ -2,14 +2,11 @@ require 'test_data/command'
 
 module TestData
   class Command < Thor
-  	desc "create [NUMBER] [SIZE]", "create test data of NUMBER SIZE"
+    desc "create [NUMBER] [SIZE]", "create the test data."
   	def create(number, size)
       number.to_i.times do |e|
-        File.open(e.to_s + ".txt", "w") do |file|
-          loop do
-            file.print 0
-            break if file.size == size.to_i
-          end
+        File.open("./testdata/" + e.to_s, "w") do |file|
+          file.truncate(size.to_i)
         end
       end
   	end
