1e8cf77911b99b53d871ac8adec9ba8e79354b11
mizutaki <mizutaki4@gmail.com>
2016-01-09 23:58:45 +0900

    add the test of create method

--- lib/test_data/command/create.rb
+++ lib/test_data/command/create.rb
@@ -2,13 +2,16 @@ require 'test_data/command'
 
 module TestData
   class Command < Thor
-    desc "create [NUMBER] [SIZE]", "create the test data."
-  	def create(number, size)
-      number.to_i.times do |e|
-        File.open("./testdata/" + e.to_s, "w") do |file|
-          file.truncate(size.to_i)
+    desc "create --num [NUMBER] --size [SIZE]", "create the test data."
+    method_options :num => :numeric, :size => :numeric
+    def create
+      path = "./testdata/"
+      FileUtils.mkdir_p(path) unless File.directory?(path)
+      options[:num].times do |n|
+        File.open(path + n.to_s, "w") do |file|
+          file.truncate(options[:size])
         end
       end
-  	end
+    end
   end
 end
--- spec/test_data_spec.rb
+++ spec/test_data_spec.rb
@@ -1,11 +1,28 @@
 require 'spec_helper'
+require 'fileutils'
 
 describe TestData do
-  it 'has a version number' do
-    expect(TestData::VERSION).not_to be nil
+  describe 'version' do
+    it 'has a version number' do
+      expect(TestData::VERSION).not_to be nil
+    end
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'create' do
+    after :each do
+      FileUtils.rm_r(Dir.glob('./testdata'), {:force=>true})
+    end
+
+    it "should one test data size 1byte" do
+      TestData::Command.new.invoke(:create, [],{:num => 1, :size => 1})
+      expect(1).to eq File.size("./testdata/0")
+      expect(1).to eq Dir.glob("./testdata/*").length
+    end
+
+    it "should thousand test data size 1000byte" do
+      TestData::Command.new.invoke(:create, [],{:num => 1000, :size => 1000})
+      expect(1000).to eq File.size("./testdata/0")
+      expect(1000).to eq Dir.glob("./testdata/*").length
+    end
   end
 end
