11075037497aaef76f4e27ded09edf0f77e02751
mizutaki <mizutaki4@gmail.com>
2016-01-10 07:44:51 +0900

    add test and refactoring

--- lib/test_data/command/create.rb
+++ lib/test_data/command/create.rb
@@ -5,6 +5,7 @@ module TestData
     desc "create --num [NUMBER] --size [SIZE]", "create the test data."
     method_options :num => :numeric, :size => :numeric
     def create
+      raise ArgumentError, "argument is nil" if options[:num] == nil || options[:size] == nil
       path = "./testdata/"
       FileUtils.mkdir_p(path) unless File.directory?(path)
       options[:num].times do |n|
--- spec/test_data_spec.rb
+++ spec/test_data_spec.rb
@@ -9,20 +9,33 @@ describe TestData do
   end
 
   describe 'create' do
+    before :each do
+      @path = "./testdata/*"
+    end
+
     after :each do
       FileUtils.rm_r(Dir.glob('./testdata'), {:force=>true})
     end
 
+    it "should no arguments" do
+      expect{TestData::Command.new.invoke(:create)}.to raise_error("argument is nil")
+    end
+
+    it "should zero test data size 0byte" do
+      TestData::Command.new.invoke(:create, [],{:num => 0, :size => 0})
+      expect(Dir.glob(@path).length).to eq 0
+    end
+
     it "should one test data size 1byte" do
       TestData::Command.new.invoke(:create, [],{:num => 1, :size => 1})
-      expect(1).to eq File.size("./testdata/0")
-      expect(1).to eq Dir.glob("./testdata/*").length
+      expect(File.size("./testdata/0")).to eq 1
+      expect(Dir.glob(@path).length).to eq 1
     end
 
     it "should thousand test data size 1000byte" do
       TestData::Command.new.invoke(:create, [],{:num => 1000, :size => 1000})
-      expect(1000).to eq File.size("./testdata/0")
-      expect(1000).to eq Dir.glob("./testdata/*").length
+      expect(File.size("./testdata/0")).to eq 1000
+      expect(Dir.glob(@path).length).to eq 1000
     end
   end
 end
